c89f6d636c4c262988f09ef40cc78052295197a2
apokkalyps <32598875+apokkalyps@users.noreply.github.com>
2019-01-17 16:08:42 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,69 +1,73 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include "main.h"
+#include "Tests.h"
 
-struct Restrictions
-// extensions indique si l'option "-e" est demandee.
-// heure indique l'heure demandee par l'utilisateur. -1 signifie pas d'heure demandee.
-{
-	bool extensions;
-	unsigned char heure;
-	Restriction (bool e, unsigned char h) : extension (e), heure (h) {}
-};
+using namespace std;
+
+Restrictions :: Restrictions (bool e, unsigned char h) : extensions (e), heure (h) {}
 
 int main ( int argc, char *argv[])
 {
+#ifdef MAP
+    cout << "Appel du main avec les parametres suivants : " << endl;
+    AfficheArgs(argv, argc);
+#endif // MAP
+
 	// Definitions prealables
 	Restrictions restr (false, -1);
 	string source ( argv [argc--]);
 	string graphviz;
-	
+
 	// Lecture des arguments
 	for (int i=1; i<argc; i++)
 	{
-		if (argv [i] == "-e")
+		if ( ! strcmp(argv [i], "-e"))
 		{
 			if (restr.extensions == true)
 			{
 				cerr << "Trop d'options : \"-e\" deja demande." << endl;
 				exit (1);
 			}
-			
+
 			restr.extensions = true;
-		} else if (argv [i] == ("-g")
+		} else if ( ! strcmp( argv [i], "-g"))
 		{
 			if ( ! graphviz.empty () )
 			{
 				cerr << "Trop d'options : \"-g\" deja demande." << endl;
 				exit (1);
 			}
-			
+
 			graphviz = argv [i+1];
 			++i;
-		} else if (argv [i] == "-t")
+		} else if ( ! strcmp( argv [i], "-t"))
 		{
 			if ( restr.heure != -1 )
 			{
 				cerr << "Trop d'options : \"-t\" deja demande." << endl;
 				exit (1);
 			}
-			
+
 			++i;
-			unsigned int h = stoi (argv[i]);
+			unsigned int h = (unsigned int) stoi (argv[i]);
 			if (h > 23)
 			{
 				cerr << "Heure demandee invalide." << endl;
 				exit (1);
 			}
-			
+
 			restr.heure = h;
 		} else
 		{
 			cerr << "Erreur, option invalide." << endl;
 		}
 	}
-			   
-	// Verification des droits sur les fichiers.		   
+
+	// Verification des droits sur les fichiers.
 	{
 		ifstream ifs (source.c_str());
 		if ( ! ifs )
@@ -71,7 +75,7 @@ int main ( int argc, char *argv[])
 			cerr << "Echec de lecture du fichier log !" << endl;
 			exit (2);
 		}
-		
+
 		if ( ! graphviz.empty () )
 		{
 			ofstream ofs (graphviz.c_str(), ios_base::app);
@@ -82,9 +86,9 @@ int main ( int argc, char *argv[])
 			}
 		}
 	}
-			   
+
 	// Lecture du fichier d'entree
-	
+
 	// Production de la sortie
 	if (graphviz.empty())
 	{
@@ -95,4 +99,11 @@ int main ( int argc, char *argv[])
 		GenererGraphe();
 	}
 	return 0;
+}
+
+std::ostream & operator << (ostream & os, const Restrictions & res)
+{
+    os << boolalpha << "ext=" << res.extensions << ", heure=";
+    //os << ((res.heure > 23) ? string("undefined") : to_string(res.heure)) << '.';
+    return os;
 }
