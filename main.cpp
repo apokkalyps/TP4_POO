2c7a415a2994f525140787334961b66545306c38
apokkalyps <32598875+apokkalyps@users.noreply.github.com>
2019-01-17 17:04:02 +0100

    module main plus joli

--- main.cpp
+++ main.cpp
@@ -1,31 +1,47 @@
+/*************************************************************************
+                           Main  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation du module <Main> (fichier Main.cpp) ---------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//-------------------------------------------------------- Include système
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <cstdlib>
 #include <cstring>
-#include "main.h"
-#include "Tests.h"
-
 using namespace std;
 
-Restrictions :: Restrictions (bool e, unsigned char h) : extensions (e), heure (h) {}
+//------------------------------------------------------ Include personnel
+#include "Main.h"
+#include "Tests.h"
 
-int main ( int argc, char *argv[])
+///////////////////////////////////////////////////////////////////  PRIVE
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//---------------------------------------------------- Variables statiques
+static string graphviz;
+static string source;
+static Restrictions restr;
+//------------------------------------------------------ Fonctions privées
+static void LectureArguments ( char ** args, int nbr_args)
+// Mode d'emploi :
+//	Traite les options saisies par l'utilisateur lors de l'appel du programme.
+//	args : liste des paramètres tels que fournis lors de l'appel du main.
+// 	nbr_args : nombre de paramètres tel que fourni lors de l'appel du main.
+// Contrat : 
+//	restr a été construit par défaut, nbr_args est compatible avec args.
 {
-#ifdef MAP
-    cout << "Appel du main avec les parametres suivants : " << endl;
-    AfficheArgs(argv, argc);
-#endif // MAP
-
-	// Definitions prealables
-	Restrictions restr (false, -1);
-	string source ( argv [argc--]);
-	string graphviz;
-
-	// Lecture des arguments
-	for (int i=1; i<argc; i++)
+	for (int i=1; i<nbr_args; i++)
 	{
-		if ( ! strcmp(argv [i], "-e"))
+		if ( ! strcmp(args [i], "-e"))
 		{
 			if (restr.extensions == true)
 			{
@@ -34,7 +50,7 @@ int main ( int argc, char *argv[])
 			}
 
 			restr.extensions = true;
-		} else if ( ! strcmp( argv [i], "-g"))
+		} else if ( ! strcmp( args [i], "-g"))
 		{
 			if ( ! graphviz.empty () )
 			{
@@ -42,9 +58,9 @@ int main ( int argc, char *argv[])
 				exit (1);
 			}
 
-			graphviz = argv [i+1];
+			graphviz = args [i+1];
 			++i;
-		} else if ( ! strcmp( argv [i], "-t"))
+		} else if ( ! strcmp( args [i], "-t"))
 		{
 			if ( restr.heure != -1 )
 			{
@@ -53,7 +69,7 @@ int main ( int argc, char *argv[])
 			}
 
 			++i;
-			unsigned int h = (unsigned int) stoi (argv[i]);
+			unsigned int h = (unsigned int) stoi (args[i]);
 			if (h > 23)
 			{
 				cerr << "Heure demandee invalide." << endl;
@@ -66,26 +82,65 @@ int main ( int argc, char *argv[])
 			cerr << "Erreur, option invalide." << endl;
 		}
 	}
+}
 
-	// Verification des droits sur les fichiers.
+static void VerificationDroits (ifstream * ifs, ofstream * ofs)
+// Mode d'emploi :
+//	Vérifie les droits d'accès aux fichiers.
+//	Crée aussi les flux si besoin.
+// Contrat :
+//	Aucun !
+{
+	ifs = new ifstream(source.c_str());
+	if ( ! *ifs)
 	{
-		ifstream ifs (source.c_str());
-		if ( ! ifs )
+		cerr << "Echec de lecture du fichier log !" << endl;
+		exit (2);
+	}
+
+	if ( ! graphviz.empty () )
+	{
+		ofs = new ofstream (graphviz.c_str(), ios_base::app);
+		if ( ! *ofs )
 		{
-			cerr << "Echec de lecture du fichier log !" << endl;
+			cerr << "Echec d'ouverture du fichier " << graphviz << '.' << endl;
 			exit (2);
 		}
-
-		if ( ! graphviz.empty () )
-		{
-			ofstream ofs (graphviz.c_str(), ios_base::app);
-			if ( ! ofs )
-			{
-				cerr << "Echec d'ouverture du fichier " << graphviz << '.' << endl;
-				exit (2);
-			}
-		}
 	}
+}
+
+static void AfficheTop10 ()
+{
+
+} //----- Fin de AfficheTop10.
+
+static void GenererGraphe ()
+{
+
+} //----- Fin de GenererGraphe.
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+int main ( int argc, char *argv[])
+{
+#ifdef MAP
+    cout << "Appel du main avec les parametres suivants : " << endl;
+    AfficheArgs(argv, argc);
+#endif // MAP
+
+	// Definitions prealables
+	restr = Restrictions (false, -1);
+	source = string ( argv [--argc]);
+	graphviz = string ( "" );
+
+	// Lecture des arguments
+	LectureArguments (argv, argc);
+
+	// Verification des droits sur les fichiers.
+	ifstream * ifs_p = nullptr;
+	ofstream * ofs_p = nullptr;
+	VerificationDroits(ifs_p, ofs_p);
+	
 
 	// Lecture du fichier d'entree
 
@@ -99,11 +154,16 @@ int main ( int argc, char *argv[])
 		GenererGraphe();
 	}
 	return 0;
-}
+} //----- fin de Main.
+
+Restrictions :: Restrictions (bool e, unsigned char h) : 
+	extensions (e), heure (h) 
+{
+} // ----- fin du constructeur de Restrictions.
 
 std::ostream & operator << (ostream & os, const Restrictions & res)
 {
-    os << boolalpha << "ext=" << res.extensions << ", heure=";
-    //os << ((res.heure > 23) ? string("undefined") : to_string(res.heure)) << '.';
+    os << boolalpha << "[ext=" << res.extensions << ", heure=";
+    os << ((res.heure > 23) ? string("?") : to_string(res.heure)) << ']';
     return os;
-}
+} //----- fin de Operator<< pour Restrictions.
--- main.h
+++ main.h
@@ -1,24 +1,56 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+/*************************************************************************
+                           Main  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
 
+//---------- Interface du module <Main> (fichier Main.h) -------------------
+#if ! defined ( MAIN_H )
+#define MAIN_H
+
+//------------------------------------------------------------------------
+// Rôle du module <Main>
+//	Module lancé au démarrage du programme.
+//	Il prend en compte les arguments puis appelle tour à tour les autres
+//	fonctionnalités.
+//------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//--------------------------------------------------- Interfaces utilisées
 #include <iostream>
+#include <string>
 
-void AfficheTop10() {}
-void GenererGraphe() {}
+//------------------------------------------------------------- Constantes
 
+//------------------------------------------------------------------ Types
 struct Restrictions
-// extensions indique si l'option "-e" est demandee.
-// heure indique l'heure demandee par l'utilisateur. -1 signifie pas d'heure demandee.
+// Structure encapsulant (en public) les requêtes de l'utilisateurs
+// en matière de restrictions dans la lecture du fichier.
+// > extensions indique si l'option "-e" est demandee.
+// > heure indique l'heure demandee par l'utilisateur. 
+// 		-1 signifie pas d'heure demandee.
 {
 	bool extensions;
 	unsigned char heure;
-	Restrictions (bool e, unsigned char h);
+	Restrictions (bool e = false, unsigned char h = -1); // Constructeur
 };
 
-std::ostream & operator << (std::ostream & os, const Restrictions & res);
-
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
 int main ( int argc, char *argv[]);
+// Mode d'emploi :
+//	Fonction Main, la seule appellable depuis l'extérieur.
+// 	Toutes les autres fonctionnalités sont encapsulées.
+// Contrat :
+//	Aucun !
 
+std::ostream & operator << (ostream & os, const Restrictions & res);
+// Mode d'emploi :
+//  Surcharge de l'opérateur d'injection dans un flux.
+// Contrat :
+//  Aucun !
 
+#endif // MAIN_H
 
-#endif // MAIN_H_INCLUDED
